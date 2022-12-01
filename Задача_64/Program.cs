5928ae4356487723cdd5458d4de4c4d2e51c9d3c
Vadim Fevralev <fevralev.vadim@mail.ru>
2022-12-01 12:04:31 +0300

    решили задачу 64

---
+++ Задача_64/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+String NaturalNumbers(int N)
+{
+    if (N == 1) return N.ToString();
+    return $"{N}, {NaturalNumbers(N - 1)}";
+}
+
+
+int[] data = new int[] { 5, 8 };
+foreach (int item in data)
+{
+    Console.WriteLine($"N = {item,2} ->  \"{NaturalNumbers(item)}\"");
+}
